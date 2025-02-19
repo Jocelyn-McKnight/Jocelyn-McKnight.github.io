cf9b95c9be79ff6d43b2f9eb8565d297374a9a69
Jocelyn-McKnight <167371795+Jocelyn-McKnight@users.noreply.github.com>
2025-02-19 22:58:50 +0000

    Deploying to gh-pages from @ Jocelyn-McKnight/Jocelyn-McKnight.github.io@5479011854749e10297d56e1d43820e6a86b3703 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -89,7 +89,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%6F%63%65%6C%79%6E.%6D%63%6B%6E%69%67%68%74@%75%6C%65%74%68.%63%61", "_blank");
         },
       },{
         id: 'social-github',
@@ -97,13 +97,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://github.com/Jocelyn-McKnight", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
         id: 'social-linkedin',
@@ -118,20 +111,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://orcid.org/0009-0006-5636-2854", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
