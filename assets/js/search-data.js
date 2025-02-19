9822caf529dc6ed00a71ee7609197214f5e13adc
Jocelyn-McKnight <167371795+Jocelyn-McKnight@users.noreply.github.com>
2025-02-19 06:27:11 +0000

    Deploying to gh-pages from @ Jocelyn-McKnight/Jocelyn-McKnight.github.io@d09dc9b605f07113a3686dd683ea5e3aabfc8017 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -95,7 +95,7 @@ ninja.data = [{
           title: 'The Visionary Cross Project',
           description: "Research Assistant",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/viscross/";
+              window.location.href = "/projects/vc_project/";
             },},{
         id: 'social-email',
         title: 'email',
